177653b6d681e2ed8750352f4706cee4403faedb
Deathblade <92017985+Gienoviere@users.noreply.github.com>
2026-03-17 12:24:00 +0100

    Update vite.config.js

--- animalcare-frontend/vite.config.js
+++ animalcare-frontend/vite.config.js
@@ -1,17 +1,13 @@
 import { defineConfig } from "vite";
-import { createHtmlPlugin } from "vite-plugin-html";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [
-    createHtmlPlugin({
-      minify: false,
-      pages: [
-        {
-          entry: "src/inventory/main.js",
-          filename: "inventory/index.html",
-          template: "src/inventory/index.html",
-        },
-      ],
-    }),
-  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index3.html"),
+        inventory: resolve(__dirname, "src/inventory/index.html"),
+      },
+    },
+  },
 });
