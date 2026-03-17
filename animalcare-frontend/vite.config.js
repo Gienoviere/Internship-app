cfdb14d093514772c74b404b64d415a39d2ff7d2
Deathblade <92017985+Gienoviere@users.noreply.github.com>
2026-03-17 12:15:44 +0100

    Update vite.config.js

--- animalcare-frontend/vite.config.js
+++ animalcare-frontend/vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
+        main: resolve(__dirname, "index3.html"),
         inventory: resolve(__dirname, "src/inventory/index.html"),
       },
     },
