56007086567ec0a80c47f4536358ca9e966c56bd
Deathblade <92017985+Gienoviere@users.noreply.github.com>
2026-03-18 12:39:30 +0100

    Update vite.config.js

--- animalcare-frontend/vite.config.js
+++ animalcare-frontend/vite.config.js
@@ -1,16 +1,20 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index3.html"),
+        main: resolve(__dirname, "index.html"),
         inventory: resolve(__dirname, "src/inventory/index.html"),
         tasks: resolve(__dirname, "Tasks/tasks.html"),
         observations: resolve(__dirname, "Observations/Observations.html"),
-        users: resolve(__dirname, "AccountCreation/users.html")
-      }
-    }
-  }
+        users: resolve(__dirname, "AccountCreation/users.html"),
+      },
+    },
+  },
 });
