27e21c1ba15ebb2f1a21c52ec0f2b5dd60c7f0c0
Deathblade <92017985+Gienoviere@users.noreply.github.com>
2026-03-23 10:38:48 +0100

    Update api.js

--- animalcare-frontend/js/api.js
+++ animalcare-frontend/js/api.js
@@ -1,15 +1,9 @@
 import { getToken, clearToken } from "./state.js";
 
-// const API_BASE_URL = (
-//   (typeof import.meta !== "undefined" && import.meta.env && import.meta.env.VITE_API_BASE_URL)
-//   || "https://internship-app-4iud.onrender.com"
-// ).replace(/\/$/, "");
-
 const API_BASE_URL = (
   (typeof import.meta !== "undefined" && import.meta.env && import.meta.env.VITE_API_BASE_URL)
   || "http://192.168.20.40:3001"
 ).replace(/\/$/, "");
-
 
 try {
   const apiHost = new URL(API_BASE_URL).hostname;
